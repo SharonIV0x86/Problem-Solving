eb96f790224229357605006f9a97841aacf1c828
SharonIV0x86 <anirudhlakhanpal123@gmail.com>
2024-07-07 23:54:15 +0530

    + leetcode, kadane's algorithm

---
+++ leetcode/medium/maximum-subarray-sum-kadane-algorithm.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int n = nums.size();
+        int maxElement = INT_MIN;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            if(sum > maxElement){
+                maxElement = sum;
+            }if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxElement;
+    }
+};
